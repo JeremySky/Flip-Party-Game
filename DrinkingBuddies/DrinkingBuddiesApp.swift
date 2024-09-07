88c227dbf44d882e1243144002743b2a578a8610
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2024-09-06 20:59:04 -0700

    Initial Commit

---
+++ DrinkingBuddies/DrinkingBuddiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrinkingBuddiesApp.swift
+//  DrinkingBuddies
+//
+//  Created by Jeremy Manlangit on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DrinkingBuddiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
