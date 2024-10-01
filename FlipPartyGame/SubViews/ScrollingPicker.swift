1ad9610f4ad2714961fc5b44a0d6aa9e71c398d5
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2024-09-30 22:14:10 -0700

    Rename ImageSelection to IconSelection

--- FlipPartyGame/SubViews/ScrollingPicker.swift
+++ FlipPartyGame/SubViews/ScrollingPicker.swift
@@ -9,92 +9,6 @@ import SwiftUI
 
 enum ItemType { case color(Color), image(String) }
 
-enum ColorSelection: CaseIterable {
-    case red, pink, orange, yellow, green, mint, teal, cyan, blue, purple, indigo, brown, black
-    
-    private var value: Color {
-        switch self {
-        case .red:
-            Color.red
-        case .pink:
-            Color.pink
-        case .orange:
-            Color.orange
-        case .yellow:
-            Color.yellow
-        case .green:
-            Color.green
-        case .mint:
-            Color.mint
-        case .teal:
-            Color.teal
-        case .cyan:
-            Color.cyan
-        case .blue:
-            Color.blue
-        case .purple:
-            Color.purple
-        case .indigo:
-            Color.indigo
-        case .brown:
-            Color.brown
-        case .black:
-            Color.black
-        }
-    }
-    
-    static var collection: [ItemType] {
-        
-            var arr: [ItemType] = []
-            
-            for color in ColorSelection.allCases {
-                arr.append(.color(color.value))
-            }
-            
-            return arr
-    }
-}
-
-enum ImageSelection: CaseIterable {
-    case beerBlue, beerRed, beerPurple, beerCan, beerPong, cocktailPink, cocktailGreen, strong, iceBucket, barrel
-    
-    private var string: String {
-        switch self {
-        case .beerBlue:
-            "beer-blue"
-        case .beerRed:
-            "beer-red"
-        case .beerPurple:
-            "beer-purple"
-        case .beerCan:
-            "beer-can"
-        case .beerPong:
-            "beer-pong"
-        case .cocktailPink:
-            "cocktail-pink"
-        case .cocktailGreen:
-            "cocktail-green"
-        case .strong:
-            "strong"
-        case .iceBucket:
-            "ice-bucket"
-        case .barrel:
-            "barrel"
-        }
-    }
-    
-    static var collection: [ItemType] {
-        
-            var arr: [ItemType] = []
-            
-            for image in ImageSelection.allCases {
-                arr.append(.image(image.string))
-            }
-            
-            return arr
-    }
-}
-
 
 struct ScrollingPicker: View {
     
@@ -136,6 +50,6 @@ struct ScrollingPicker: View {
 #Preview {
     return VStack {
         ScrollingPicker(collection: ColorSelection.collection) { _ in }
-        ScrollingPicker(collection: ImageSelection.collection) { _ in }
+        ScrollingPicker(collection: IconSelection.collection) { _ in }
     }
 }
