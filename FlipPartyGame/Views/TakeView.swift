0d711c41f6778a22e5f8a2a81d2cee830de7f95b
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2024-10-12 16:05:46 -0700

    Clean up code:
    rotation effect flattened out
    Start and Continue buttons condensed into actionButton view builder

--- FlipPartyGame/Views/TakeView.swift
+++ FlipPartyGame/Views/TakeView.swift
@@ -12,9 +12,10 @@ struct TakeView: View {
     let user: User = .test1
     @State var count: Int = 10
     
+    // ANIMATION PROPERTIES...
+    @State var countdownHasBegun: Bool = false
     @State var degree: Double = 0
     private let durationAndDelay: Double = 0.19
-    @State var countdownHasBegun: Bool = false
     
     var body: some View {
         VStack {
@@ -25,52 +26,55 @@ struct TakeView: View {
                 Circle()
                     .frame(width: 215)
                     .foregroundStyle(user.color.value)
-                    .overlay(Circle().foregroundStyle(.black.opacity(0.2)))
+                    .overlay(Circle().foregroundStyle(LinearGradient(colors: [.black.opacity(0.4), .clear], startPoint: .center, endPoint: .bottom))
+                    )
+                    .rotation3DEffect(.degrees(degree), axis: (x: 0.0, y: 1.0, z: 0.0))
+                
                 Text("\(count)")
                     .font(.system(size: 140, weight: .heavy, design: .rounded))
                     .foregroundStyle(.white)
+                    .rotation3DEffect(.degrees(degree), axis: (x: 0.0, y: 1.0, z: 0.0))
             }
-            .rotation3DEffect(.degrees(degree), axis: (x: 0.0, y: 1.0, z: 0.0))
             
             Spacer()
-            ZStack {
-                Button("Start", action: {
-                    startCountdown()
-                })
-                .buttonStyle(ColorBackground(!countdownHasBegun, user.color.value))
-                .opacity(countdownHasBegun ? 0 : 1)
-                .zIndex(countdownHasBegun ? 0 : 1)
-                
-                Button("Continue", action: {
-                    // Next
-                })
+            
+            actionButtons()
+                .padding(.horizontal, 40)
+        }
+    }
+    
+    @ViewBuilder
+    private func actionButtons() -> some View {
+        if countdownHasBegun {
+            Button("Continue", action: {})
                 .buttonStyle(ColorBackground(count == 0, user.color.value))
-                .zIndex(countdownHasBegun ? 1 : 0)
-            }
+        } else {
+            Button("Start", action: { startCountdown() })
+                .buttonStyle(ColorBackground(!countdownHasBegun, user.color.value))
         }
     }
     
     private func startCountdown() {
+        
         countdownHasBegun = true
+        
         Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
             countdown(timer)
         }
+        
+        func countdown(_ timer: Timer) {
+            guard count > 0 else { return timer.invalidate() }
+            
+            withAnimation(.linear(duration: durationAndDelay)) { degree = -90 }
+            
+            DispatchQueue.main.asyncAfter(deadline: (.now() + durationAndDelay)) {
+                degree = 90
+                count -= 1
+                withAnimation(.linear(duration: durationAndDelay)) { degree = 0 }
+            }
+        }
     }
     
-    private func countdown(_ timer: Timer) {
-        guard count > 0 else {
-            timer.invalidate()
-            // Backend - count
-            return
-        }
-        
-        withAnimation(.linear(duration: durationAndDelay)) { degree = -90 }
-        DispatchQueue.main.asyncAfter(deadline: (.now() + durationAndDelay)) {
-            degree = 90
-            count -= 1
-            withAnimation(.linear(duration: durationAndDelay)) { degree = 0 }
-        }
-    }
 }
 
 #Preview {
