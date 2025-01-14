a481e80c3d636a066fbd4b089dba186270dd9f20
JeremySky <86520270+JeremySky93@users.noreply.github.com>
2025-01-13 23:53:58 -0800

    Add GameView

--- FlipPartyGame/Views/GameView.swift
+++ FlipPartyGame/Views/GameView.swift
@@ -7,36 +7,60 @@
 
 import SwiftUI
 
-enum GameState {
-    case standby, playerTurn, give, take
+enum PlayersGameState {
+    case waiting, standby, playerTurn, give, take, results
 }
 
+
 struct GameView: View {
-    @StateObject var gameManager: GameManager = GameManager.preview(cardIndex: 2)
-    @State var gameState: GameState = .standby
+    @StateObject var gameManager: GameManager
+    @State var gameState: PlayersGameState = .waiting
+    @State var user = User.test1
     
     var body: some View {
         ZStack {
             switch gameState {
+            case .waiting:
+                WaitingRoomView()
+                
             case .standby:
                 StandbyView()
+                
             case .playerTurn:
-                switch gameManager.phase {
-                case .question:
-                    GuessingView(viewModel: GuessingViewModel(gameManager: gameManager, user: gameManager.currentPlayer))
-                case .giveTake(let remainingStickers):
-                    GiveTakeSelectionView(viewModel: GiveTakeSelectionViewModel(gameManager: gameManager, remainingStickers: remainingStickers, user: gameManager.currentPlayer), user: gameManager.currentPlayer)
-                }
+                playerTurnView
+                
             case .give:
                 GiveView()
+                
             case .take:
                 TakeView()
+                
+            case .results:
+                Text("Results View")
             }
+            
         }
         .environmentObject(gameManager)
+        .environment(\.user, user)
+    }
+    
+    
+    
+    
+    private var playerTurnView: some View {
+        Group {
+            switch gameManager.phase {
+            case .question:
+                GuessingView(viewModel: GuessingViewModel(gameManager: gameManager))
+            case .giveTake:
+                GiveTakeSelectionView(viewModel: GiveTakeSelectionViewModel(gameManager: gameManager))
+            default:
+                Text("Unknown Phase")
+            }
+        }
     }
 }
 
 #Preview {
-    GameView()
+    GameView(gameManager: GameManager.preview)
 }
